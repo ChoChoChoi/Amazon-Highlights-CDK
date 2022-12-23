fd737a8862796171be265507b3edf86f39337b63
seungwon2 <dandelionsw2@ewhain.net>
2022-12-23 18:25:00 +0900

    :sparkles: Feat : add lambda folder

--- lib/amazon-highlights-cdk-stack.ts
+++ lib/amazon-highlights-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class AmazonHighlightsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
